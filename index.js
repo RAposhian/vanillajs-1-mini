45f8c92ef74bc10dcc9bf006723ff0a61d6be367
Raposhian <ryan.aposhian@gmail.com>
2020-02-17 19:13:50 -0700

    finished this part

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('Hello World!');
 
 let count = 0;
 let counterView = document.getElementById('counter');
-
+;
 
 function increase(){
    return counterView.innerText = count += 1;
@@ -20,3 +20,13 @@ function reset() {
    return counterView.innerHTML = count = 0;
 }
  
+function selectTheme(theme) {
+   document.getElementsByTagName('body')[0].className = theme;
+   document.getElementsByTagName('main')[0].className = theme;
+
+   const buttons = document.getElementsByTagName('button')
+
+   for(let i = 0; i < buttons.length; i++) {
+      buttons[i].className = theme;
+   }
+}
